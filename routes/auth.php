ba0604dec7e1ed01a3ba25307bfebec0da824745
Vlad Maslyk <88619892+Kowalski1304@users.noreply.github.com>
2024-03-18 11:47:43 +0200

    create auth file

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware('guest')
     ->group(function () {
 
